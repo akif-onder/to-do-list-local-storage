9b00372302dcf0c87b3dd583a452ca7451f6c103
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-06-09 23:37:48 +0300

    dom manipulation added

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
 const form = document.querySelector('.todo_form');
 const input = document.querySelector('.todo_input');
 
+const todoContainer = document.querySelector('.todo_container');
+
+const addHTML = (todo) =>{
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const todoLeft = document.createElement('div');
+    todoLeft.classList.add('todo_left');
+    
+    const todoCheckBox = document.createElement('input');
+    todoCheckBox.type = 'checkbox';
+    todoCheckBox.checked = todo.isCompleted;
+    todoCheckBox.classList.add('todo_checkbox');
+
+    const todoText = document.createElement('span');
+    todoText.classList.add('todo_text');
+    todoText.textContent = todo.text;
+
+    todoLeft.appendChild(todoCheckBox);
+    todoLeft.appendChild(todoText);
+
+    const todoRight = document.createElement('div');
+    todoRight.classList.add('todo_right');
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('todo_delete');
+    deleteBtn.textContent = 'Delete';
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('todo_edit');
+    editBtn.textContent = 'Edit';
+
+    todoRight.appendChild(deleteBtn);
+    todoRight.appendChild(editBtn);
+    
+
+
+    todoDiv.appendChild(todoLeft);
+    todoDiv.appendChild(todoRight);
+
+    todoContainer.appendChild(todoDiv);
+};
 
 const startConf = () => {
     //This function will operate and get items from the local storage when the page first loaded.
 
     // Start configs below...
-    const todos =localStorage.getItem('todos');
+    const todos =JSON.parse(localStorage.getItem('todos'));
 
     if (!todos) {
         localStorage.setItem('todos', JSON.stringify([]));
+    }else{
+        todos.forEach(todo => {
+            addHTML(todo);
+        });
     }
 };
 
@@ -18,15 +64,26 @@ startConf();
 const addTodo = (e) => {
     e.preventDefault(); // when a form is submitted the page is reloaded by default. this function prevents this behavior.
     
-    todoText = input.value;
+   const inputValue = input.value;
 
     const todo = {
-        text: todoText,
+        text: inputValue,
         isCompleted: false,
     }
 
-    const todos = localStorage.getItem('todos');
-   
+    const todos = JSON.parse(localStorage.getItem('todos'));
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+        
+
+   addHTML(todo);
+
+   form.reset();
+
+
+    
+    
 
 };
 
